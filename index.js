c4861748bb3ddf7e4b5144aaca0e3515b8e79f4f
Tom Nielsen <tanielsen@gmail.com>
2023-11-02 16:30:23 +0000

    some work on executable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const parser = require("./lib/parsers/postgresql_conf");
 const fs = require("fs");
-const helpText = `patchcfg [-hvpo] file patch-expr
+const helpText = `patchcfg [-hvpo] file [patch-expr]
 
 Command line switches:
 
@@ -39,6 +39,9 @@ const getArgs = () => {
           console.log(helpText);
           process.exit(0);
           break;
+        case "-o-":
+          flags.outfile = "-";
+          break;
         case "-o":
           flags.outfile = myArgs[i + 1];
           i++;
@@ -60,12 +63,25 @@ const getArgs = () => {
   return { flags, file, patchExpr };
 };
 
+const patch = (obj, expr) => {
+  return obj;
+};
 (async () => {
   const { flags, file, patchExpr } = getArgs();
   if (flags.verbose) console.log({ flags, file, patchExpr });
   const fileStr = fs.readFileSync(file).toString();
+  const parsed = parser.parser.from(fileStr);
   if (!patchExpr) {
-    console.log(parser.parser.from(fileStr));
+    console.log(parsed);
   } else {
+    const patched = patch(parsed, patchExpr);
+    const output = parser.parser.to(patched);
+    if (flags.outfile === "-") {
+      console.log(output);
+    } else if (flags.outfile) {
+      fs.writeFileSync(flags.outfile, output);
+    } else {
+      fs.writeFileSync(file, output);
+    }
   }
 })();
